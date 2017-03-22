9e78fbb63346a7c44b75fd247ed561c946fd87e3
ko1106 <okuno_ac_jp@yahoo.co.jp>
2017-03-22 16:39:32 +0900

    Create MyHomeworkBean.java

---
+++ MyHomeworkBean.java
@@ -0,0 +1,38 @@
+public class MyHomeworkBean {
+	private String name;
+	private int value1;
+	private int value2;
+	private int ans;
+	
+	public int getAns() {
+		return ans;
+	}
+	public void setAns(int ans) {
+		this.ans = ans;
+	}
+	MyHomeworkBean(){}
+	public MyHomeworkBean(String name, int value1, int value2, int ans) {
+		this.name = name;
+		this.value1 = value1;
+		this.value2 = value2;
+		this.ans=ans;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getValue1() {
+		return value1;
+	}
+	public void setValue1(int value1) {
+		this.value1 = value1;
+	}
+	public int getValue2() {
+		return value2;
+	}
+	public void setValue2(int value2) {
+		this.value2 = value2;
+	}
+}
